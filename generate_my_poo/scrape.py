4bcada1bc32a4b4694fee01346098b701c88a460
Chino Kim <chjno.k@gmail.com>
2017-03-03 19:25:16 -0500

    python is annoying

--- generate_my_poo/scrape.py
+++ generate_my_poo/scrape.py
@@ -1,15 +1,22 @@
 import bs4
 import requests
+import time
 
 
 url = 'http://www.ratemypoo.com/xyzzy/search?search=10+am'
 html = requests.get(url).text
 # print html
 soup = bs4.BeautifulSoup(html, "html.parser")
-
+# print soup
 # items = soup.select('#wd_content')
-results = soup.select('center center tr')
-print results
+results = soup.select('tr')
+for result in results:
+    print '---'
+    print result.text
+    print '----'
+    # print len(result.children)
+    # for child in result.children:
+    #     print child
 
 # for result in results:
     # print result
