1f790cae66d7aaf9be2d8a9434e8dd6ed8399bc9
Chino Kim <chjno.k@gmail.com>
2017-03-21 01:28:19 -0400

    split into chunks of 1000

--- generate_my_poo/unsearched_to_txt.py
+++ generate_my_poo/unsearched_to_txt.py
@@ -1,10 +1,18 @@
 import json
+import math
 from sys import argv
 
 jsonfile = argv[1]
+outfile = argv[2]
 
 with open(jsonfile) as d:
     data = json.load(d)
 
-unsearched = ' '.join(data['unsearched']).strip().encode('utf-8')
-print unsearched
+unsearched = data['searched']
+word_count = len(unsearched)
+chunk_count = int(math.ceil(word_count / 1000.0))
+
+for i in range(chunk_count):
+    f = open('%s_%d.txt' % (outfile, i + 1), 'w')
+    f.write(' '.join(unsearched[i * 1000:(i + 1) * 1000]).strip().encode('utf-8'))
+    f.close()
